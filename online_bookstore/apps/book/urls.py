3dc0a47a35e526b8dff1469409847270a0656f89
nikolaycholakov1 <nikolaycholakovnmc@gmail.com>
2023-07-26 20:36:40 +0300

    fixed some of the urls

--- online_bookstore/apps/book/urls.py
+++ online_bookstore/apps/book/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home-page'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home-page'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('book_detail/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]
--- online_bookstore/urls.py
+++ online_bookstore/urls.py
@@ -1,17 +1,9 @@
-# book/urls.py
+# project/urls.py
 
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from online_bookstore.apps.book.views import RegisterView, HomePageView, BookDetailView
+from django.urls import path, include
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home-page'),
+    path('', include('online_bookstore.apps.book.urls')),
     path('admin/', admin.site.urls),
-
-    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
-
-    path('book_detail/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]
